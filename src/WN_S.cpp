215463b53191d8436573de98ddac07bda384dc5b
Pablo Velasco Crespo <Pablo.Velasco2@alu.uclm.es>
2022-03-07 12:58:53 +0100

    Commit inicial

---
+++ src/WN_S.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <string>
+#include <climits>
+#include <algorithm>
+#include <regex>
+
+using namespace std;
+
+string do_replace( string const & in, string const & from, string const & to )
+{
+  return std::regex_replace( in, std::regex(from), to );
+}
+
+int codificacionCategorias(string c)
+{
+    if(c == "n") return 1;
+    else if (c == "v") return 2;
+    else if (c == "a") return 3;
+    else if (c == "r") return 4;
+    else return -1;
+}
+
+int main()
+{   
+    string datos [7];
+    fstream ficheroLectura;
+    fstream ficheroEscritura;
+    size_t i = 0;
+    size_t j = 0;
+
+    cout << "Abriendo ficheros...\n";
+
+    ficheroLectura.open("../spaWN/wei_spa-30_variant.tsv", ios::in);
+    ficheroEscritura.open("../Prolog/wn_s.pl", ios::out);
+    if(!ficheroLectura.is_open() || !ficheroEscritura.is_open())
+    {
+        cout << "No se han abierto correctamente\n";
+        return 0;
+    }
+
+    cout << "Se han abierto correctamente.\n" << "Parseando a Prolog...\n";
+
+    string line;
+
+    while(getline(ficheroLectura, line))
+    {
+        i = 0;
+        char *cadena = line.data();
+        char *token =strtok(cadena, "\t");
+
+        while(token != NULL)
+        {
+            datos[i] = token;
+            token = strtok(NULL, "\t");
+            i++;
+        }
+        ficheroEscritura << "s(" << codificacionCategorias(datos[3]) << datos[2].substr(7, 8) << ",'" << do_replace(datos[0], "'", "''") << "'," << datos[3] << "," << datos[1] << ")." << endl;
+    }
+
+    cout << "Parseado correctamente.\n";
+    ficheroLectura.close();
+    ficheroEscritura.close();
+    cout << "¡Proceso finalizado!";
+
+    return 0;
+}
