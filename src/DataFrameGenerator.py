a84176d2a713edb8aa0b2e0c54319766d688f9de
Pablo Velasco Crespo <Pablo.Velasco2@alu.uclm.es>
2023-03-07 10:30:27 +0100

    Version 1.0

--- src/DataFrameGenerator.py
+++ src/DataFrameGenerator.py
@@ -1,13 +1,17 @@
 import pandas as pd
+import os
 
 idiomas = ['cat', 'eng', 'eus', 'glg', 'por', 'spa']
 
-rutaEngKafe = 'engProlog30\wn_g.pl'
-rutaAdimenSUMO = ''
-rutaBLC = ''
-rutaDomains = ''
-rutaMarks = ''
-rutaTopOntology = ''
+rutaEngKafeS = 'engProlog30\wn_s.pl'
+rutaEngKafeG = 'engProlog30\wn_g.pl'
+
+rutaAdimenSUMO = 'mcr\\AdimenSUMO'
+rutaBLC = 'mcr\\BLC'
+rutaData = 'mcr\\data'
+rutaDomains = 'mcr\\Domains'
+rutaMarks = 'mcr\\Marks'
+rutaTopOntology = 'mcr\\TopOntology'
 
 for i in idiomas:
     rutaVariant = 'mcr\\'+i+'WN\wei_'+i+'-30_variant.tsv'
@@ -158,30 +162,87 @@ for i in idiomas:
 
     print()
 
-#TODO: HACERLO CON TODOS LOS FICHEROS: AdimenSUMO, BLC, Domains, Marks, TopOntology
+#TODO: HACERLO CON TODOS LOS FICHEROS: BLC, Domains, Marks, TopOntology
 
-print('Abriendo fichero ' + rutaEngKafe)
+#AdimenSUMO
+rutaAdimenSUMO = 'mcr\\AdimenSUMO\\'
+file_list = os.listdir(rutaAdimenSUMO)
 
-ficheroEng = open('engProlog30\wn_g.pl', encoding='utf-8')
+print('Abriendo fichero ' + rutaAdimenSUMO + file_list[0])
+
+ficheroAdimenSUMO_1 = open(rutaAdimenSUMO + file_list[0], encoding='utf-8')
+
+Synset = []
+PoS = []
+wn = []
+Class = []
+Symbol = []
+
+for linea in ficheroAdimenSUMO_1:
+    linea = linea.split('\t')
+
+    Synset.append(linea[0])
+    PoS.append(linea[1])
+    wn.append(linea[2])
+    Class.append(linea[3])
+    Symbol.append(linea[4][:-1])
+
+dfAdimenSUMO_1 = pd.DataFrame({"SynsetPoS": Synset, "PoS": PoS, "WordNet": wn, "Class": Class, "Symbol": Symbol})
+dfAdimenSUMO_1.to_csv("mcrDF\\AdimenSUMO\\" + file_list[0][:-3] + "csv")
+
+ficheroAdimenSUMO_1.close()
+
+print('Abriendo fichero ' + rutaAdimenSUMO + file_list[1])
+
+ficheroAdimenSUMO_2 = open(rutaAdimenSUMO + file_list[1], encoding='utf-8')
+
+Subclass = []
+Hierarchy_1 = []
+Hierarchy_2 = []
+
+for linea in ficheroAdimenSUMO_2:
+    linea = linea.split('\t')
+
+    Subclass.append(linea[0])
+    Hierarchy_1.append(linea[1])
+    Hierarchy_2.append(linea[2][:-1])
+
+dfAdimenSUMO_2 = pd.DataFrame({"Subclass": Subclass, "Hierarchy_1": Hierarchy_1, "Hierarchy_2": Hierarchy_2})
+dfAdimenSUMO_2.to_csv("mcrDF\\AdimenSUMO\\" + file_list[1][:-3] + "csv")
+ficheroAdimenSUMO_2.close()
+print()
+
+#WN_S ERIC KAFE
+print('Abriendo fichero ' + rutaEngKafeS)
+
+ficheroEng = open('engProlog30\wn_s.pl', encoding='utf-8')
 
 lineasEng = ficheroEng.readlines()
 
-SynsetsENG = []
-GlosaENG = []
+Synset = []
+W_Num = []
+Word = []
+Type = []
+Sense = []
+Tag_Count = []
 
 for linea in lineasEng:
     linea = linea[2:-3]
-    GlosaENG.append(linea[11:-1])
-    lineaComas = linea.split(',')
-    SynsetsENG.append(int(lineaComas[0]))
+    linea = linea.split(',')
+    Synset.append(linea[0])
+    W_Num.append(linea[1])
+    Word.append(linea[2])
+    Type.append(linea[3])
+    Sense.append(linea[4])
+    Tag_Count.append(linea[5])
 
-dfEng = pd.DataFrame({"Synset":SynsetsENG, "Glosa":GlosaENG})
-dfEng.to_csv('engProlog30DF\wn_g.csv')
+dfEng = pd.DataFrame({"Synset":Synset, "W Num":W_Num, "Word":Word, "Type":Type, "Sense":Sense, "Tag Count":Tag_Count})
+dfEng.to_csv('engProlog30DF\wn_s.csv')
 
 ficheroEng.close()
 
 #WN_G ERIC KAFE
-print('Abriendo fichero ' + rutaEngKafe)
+print('Abriendo fichero ' + rutaEngKafeG)
 
 ficheroEng = open('engProlog30\wn_g.pl', encoding='utf-8')
 
---
+++ src/prueba.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+idiomas = ['cat', 'eng', 'eus', 'glg', 'por', 'spa']
+relacionesLexicas = ['ant', 'der', 'vgp', 'per', 'sa']
+
+def metodo(synset):
+    if synset in List:
+        return 1
+    else:
+        return 0
+
+for i in idiomas:
+    dfS = pd.read_csv(i+'//PrologDF//wn_s.csv', index_col=[0])
+    for rel in relacionesLexicas:
+        dfRel = pd.read_csv(i+'//PrologDF//wn_'+rel+'.csv', index_col=[0])
+
+        Synsets_count = dfS.Synset.value_counts()
+        Synsets_count_1 = Synsets_count[(Synsets_count == 1)]
+        List = list(Synsets_count_1.index)
+
+        dfRel['W_Num Source'] = dfRel['Synset Source'].apply(metodo)
+        dfRel['W_Num Target'] = dfRel['Synset Target'].apply(metodo)
+
+        dfRel.to_csv(i+'//PrologDF//wn_'+rel+'.csv')
+        ficheroEscritura = open(i+"\Prolog\wn_"+rel+".pl", "w", encoding='utf-8')
+
+        for index in dfRel.index:
+                ficheroEscritura.write(rel+"("+str(dfRel["Synset Source"][index])+","+str(dfRel["W_Num Source"][index])+","+str(dfRel["Synset Target"][index])+","+str(dfRel["W_Num Target"][index])+").\n")
+        ficheroEscritura.close()
+
+        print("Se ha terminado de ejecutar el fichero: wn_"+rel+" del idioma "+i)
+    print("Se ha terminado de ejecutar el idioma: "+i+"\n")
